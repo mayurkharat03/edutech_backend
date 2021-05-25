d60402713c36fd11f9b585185aa9729c5d17be27
mayurkharat03 <mayurkharat03@gmail.com>
2021-05-25 22:14:07 +0530

    photo json token removed

--- routers/photo.route.js
+++ routers/photo.route.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const photoController = require('../controller/users/photoController')
 const middleware = require('../utils/userAuth.middleware')
-router.post('/:id/uploadProfilePhoto', middleware.checkToken, photoController.upload, photoController.uploadProfilePhoto);
-router.post('/:id/uploadAadhaarFrontPhoto', middleware.checkToken, photoController.upload, photoController.uploadAadhaarFrontPhoto);
-router.post('/:id/uploadAadhaarBackPhoto', middleware.checkToken, photoController.upload, photoController.uploadAadhaarBackPhoto);
-router.post('/:id/uploadPancardPhoto', middleware.checkToken, photoController.upload, photoController.uploadPancardPhoto);
+router.post('/:id/uploadProfilePhoto', photoController.upload, photoController.uploadProfilePhoto);
+router.post('/:id/uploadAadhaarFrontPhoto', photoController.upload, photoController.uploadAadhaarFrontPhoto);
+router.post('/:id/uploadAadhaarBackPhoto', photoController.upload, photoController.uploadAadhaarBackPhoto);
+router.post('/:id/uploadPancardPhoto', photoController.upload, photoController.uploadPancardPhoto);
 
 
 
